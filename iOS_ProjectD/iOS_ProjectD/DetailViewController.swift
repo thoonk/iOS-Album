3f584ad85fcf57de21cd5769533db802abf6abf6
thoonk <dkrmrm878@gmail.com>
2020-09-22 23:53:01 +0900

    Add DetailViewController

--- iOS_ProjectD/iOS_ProjectD/DetailViewController.swift
+++ iOS_ProjectD/iOS_ProjectD/DetailViewController.swift
@@ -6,8 +6,14 @@
 //
 
 import UIKit
+import Photos
 
 class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var detailImage: UIImage!
+    var detailAsset: PHAsset!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,14 +22,6 @@ class DetailViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+ 
 
 }
