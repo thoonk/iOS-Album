87c1089daa7b8e65fcbf86a128342251d49a30a3
thoonk <dkrmrm878@gmail.com>
2020-09-26 13:23:31 +0900

    Add shareAction function

--- iOS_ProjectD/iOS_ProjectD/DetailViewController.swift
+++ iOS_ProjectD/iOS_ProjectD/DetailViewController.swift
@@ -27,17 +27,31 @@ class DetailViewController: UIViewController, UIScrollViewDelegate{
                                   options: nil,
                                   resultHandler: {image, _ in self.imageView.image = image})
 
-        scrollView.minimumZoomScale = 1.0
-        scrollView.maximumZoomScale = 3.0
+        
+//        scrollView.minimumZoomScale = 1.0
+//        scrollView.maximumZoomScale = 3.0
         
     }
     
+    // 네비게이션바 공유 기능 버튼
+    @IBAction func shareButtonAction(_ sender: UIBarButtonItem){
+        
+        let activityItem: [AnyObject] = [self.imageView.image as AnyObject]
+        
+        // UIActivityViewController를 통한 shareAction 구현
+        let activityViewController = UIActivityViewController(activityItems: activityItem, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+    
+    // 네비게이션바 삭제 기능 버튼
     @IBAction func trashButtonAction(_ sender: UIBarButtonItem){
         PHPhotoLibrary.shared().performChanges({PHAssetChangeRequest.deleteAssets([self.detailAsset] as NSArray)}, completionHandler: nil)
         
         self.navigationController?.popViewController(animated: true)
     }
     
+    // imageView 줌 기능
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return self.imageView
     }
